9e8a4db962053e09b6fd0fbd2684e827c5b48a46
Antonio Edinadson <antonioedinadson@gmail.com>
2020-11-05 22:24:31 -0300

    CRIANDO ESTRUTURA BASE

---
+++ Config.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require __DIR__ . "/environment.php";
+
+$config = array();
+
+if (ENVIRONMENT == 'development') {
+
+    define("BASE-URL", "http://localhost/B7web_MVC/classificados/");
+
+    $config['dbname'] = 'classificados';
+    $config['dbhost']   = 'localhost';
+    $config['dbuser'] = 'root';
+    $config['dbpass'] = '';
+} else {
+    define("BASE-URL", "http://localhost/B7web_MVC/classificados/");
+
+    $config['dbname'] = 'classificados';
+    $config['host']   = 'localhost';
+    $config['dbuser'] = 'root';
+    $config['dbpass'] = '';
+}
+
+global $db;
+try {
+
+    $db = new PDO("mysql:dbname=" . $config['dbname'] . ";host=" . $config['dbhost'], $config['dbuser'], $config['dbpass']);
+
+} catch (PDOException $e) {
+    echo "ERRO! " .  $e->getMessage();
+    exit;
+}
